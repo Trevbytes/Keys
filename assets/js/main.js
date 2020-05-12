8e05f82d0b03910fbcaa629e827fcaeac259ebb5
Trevbytes <trevor.j.adams@outlook.com>
2020-05-12 14:02:29 +0000

    Added basic structure to piano.

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+/*--------------Piano CSS code heavily influenced by a YouTube tutorial by Kyle.  https://www.youtube.com/watch?v=vjco5yKZpU8 */
+const keys = $(".key");
+
+keys.forEach((key) => {
+  key.addEventListener("click", () => playNote(key));
+});
+
+function playNote(key) {
+  const noteAudio = "C";
+  noteAudio.currentTime = 0;
+  noteAudio.play();
+  key.classList.add("active");
+  noteAudio.addEventListener("ended", () => {
+    key.classList.remove("active");
+  });
+}
