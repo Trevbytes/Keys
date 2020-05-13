0b3f42120f4b40f9fb19b1fc4b6773383a471df3
Trevbytes <trevor.j.adams@outlook.com>
2020-05-13 12:42:08 +0000

    Cleaned code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,12 @@
-/*--------------Piano CSS code heavily influenced by a YouTube tutorial by Kyle.  https://www.youtube.com/watch?v=vjco5yKZpU8 */
+/*--------------Piano js code with help by a YouTube tutorial by Kyle.  https://www.youtube.com/watch?v=vjco5yKZpU8 */
 const keys3 = document.querySelectorAll(".key3");
 const keys4 = document.querySelectorAll(".key4");
 const keys5 = document.querySelectorAll(".key5");
 
 keys3.forEach((key3) => {
-  key3.addEventListener("click", () => Synth.play(0, key3.id, 3, 2));
+  key3.addEventListener("click", () =>
+    Synth.play(0, key3.id.substr(1, 3), 3, 2)
+  );
 });
 
 keys4.forEach((key4) => {
@@ -18,31 +20,49 @@ keys5.forEach((key5) => {
     Synth.play(0, key5.id.substr(1, 3), 5, 2)
   );
 });
-keys3.forEach((key3) => {
-  key3.addEventListener("mousedown", () => {
-    key3.classList.add("active");
-  });
-  key3.addEventListener("mouseup", () => {
-    key3.classList.remove("active");
-  });
-});
-// function playNote(key3) {
-//   Synth.play(0, key3.id, 4, 2);
-//   key3.classList.add("active");
-//   key3.addEventListener("mouseup", () => {
-//     key.classList.remove("active");
-//   });
 
-// function playNote(key4) {
-//   Synth.play(0, key4.id, 4, 2);
-//   key4.classList.add("active");
-//   noteAudio.addEventListener("ended", () => {
-//     key.classList.remove("active");
-//   });
+/*--Adding and removing active class to piano keys. Help from Leesa Ward. https://gist.github.com/doubleedesign/08f89ad1c4f0ba73ffdbc727136da190 --*/
+for (var i = 0; i < keys3.length; i++) {
+  // Add 'open' class on mouseover
+  keys3[i].addEventListener("mousedown", function () {
+    this.classList.add("active");
+  });
 
-// function playNote(key5) {
-//   Synth.play(0, key5.id, 4, 2);
-//   key5.classList.add("active");
-//   noteAudio.addEventListener("ended", () => {
-//     key.classList.remove("active");
-//   });
+  // Remove 'open" class with a delay
+  keys3[i].addEventListener("mouseup", function () {
+    var node = this; // Allows us to access 'this' within the timeout function
+    setTimeout(function () {
+      node.classList.remove("active");
+    }, 300);
+  });
+}
+
+for (var i = 0; i < keys4.length; i++) {
+  // Add 'open' class on mouseover
+  keys4[i].addEventListener("mousedown", function () {
+    this.classList.add("active");
+  });
+
+  // Remove 'open" class with a delay
+  keys4[i].addEventListener("mouseup", function () {
+    var node = this; // Allows us to access 'this' within the timeout function
+    setTimeout(function () {
+      node.classList.remove("active");
+    }, 300);
+  });
+}
+
+for (var i = 0; i < keys5.length; i++) {
+  // Add 'open' class on mouseover
+  keys5[i].addEventListener("mousedown", function () {
+    this.classList.add("active");
+  });
+
+  // Remove 'open" class with a delay
+  keys5[i].addEventListener("mouseup", function () {
+    var node = this; // Allows us to access 'this' within the timeout function
+    setTimeout(function () {
+      node.classList.remove("active");
+    }, 300);
+  });
+}
