896d1cf0d45e2ba6e961337c4a48db8e183154df
Trevbytes <trevor.j.adams@outlook.com>
2020-05-14 08:07:07 +0000

    Simplified JS code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,87 +1,29 @@
-/*--------------Piano js code with help by a YouTube tutorial by Kyle.  https://www.youtube.com/watch?v=vjco5yKZpU8 */
-const keys3 = document.querySelectorAll(".key3");
-const keys4 = document.querySelectorAll(".key4");
-const keys5 = document.querySelectorAll(".key5");
+/*--------------Piano js code  */
+const keys = document.querySelectorAll(".key");
 
-keys3.forEach((key3) => {
-  key3.addEventListener("mousedown", () =>
-    Synth.play(0, key3.id.substr(1, 3), key3.id.substr(0, 1), 2)
+keys.forEach((key) => {
+  key.addEventListener("mousedown", () =>
+    Synth.play(0, key.id.substr(1, 3), key.id.substr(0, 1), 2)
   );
 });
 
-keys4.forEach((key4) => {
-  key4.addEventListener("mousedown", () =>
-    Synth.play(0, key4.id.substr(1, 3), 4, 2)
-  );
-});
-
-keys5.forEach((key5) => {
-  key5.addEventListener("mousedown", () =>
-    Synth.play(0, key5.id.substr(1, 3), 5, 2)
-  );
-});
-
-/*--Adding and removing active class to piano keys. Help from Leesa Ward. https://gist.github.com/doubleedesign/08f89ad1c4f0ba73ffdbc727136da190 --*/
-for (let i = 0; i < keys3.length; i++) {
+/*--Adding and removing active class to piano keys.  --*/
+for (let i = 0; i < keys.length; i++) {
   // Add 'open' class on mouseover
-  keys3[i].addEventListener("mousedown", function () {
+  keys[i].addEventListener("mousedown", function () {
     this.classList.add("active");
   });
 
-  // Remove 'open" class with a delay
-  keys3[i].addEventListener("mouseup", function () {
-    let node = this; // Allows us to access 'this' within the timeout function
+  // Remove 'open" class with a delay after mouseup
+  keys[i].addEventListener("mouseup", function () {
+    let node = this; // Allow access to 'this' in a timeout function
     setTimeout(function () {
       node.classList.remove("active");
     }, 300);
   });
-
-  keys3[i].addEventListener("mouseout", function () {
-    let node = this; // Allows us to access 'this' within the timeout function
-    setTimeout(function () {
-      node.classList.remove("active");
-    }, 300);
-  });
-}
-
-for (let i = 0; i < keys4.length; i++) {
-  // Add 'open' class on mouseover
-  keys4[i].addEventListener("mousedown", function () {
-    this.classList.add("active");
-  });
-
-  // Remove 'open" class with a delay
-  keys4[i].addEventListener("mouseup", function () {
-    let node = this; // Allows us to access 'this' within the timeout function
-    setTimeout(function () {
-      node.classList.remove("active");
-    }, 300);
-  });
-
-  keys4[i].addEventListener("mouseout", function () {
-    let node = this; // Allows us to access 'this' within the timeout function
-    setTimeout(function () {
-      node.classList.remove("active");
-    }, 300);
-  });
-}
-
-for (let i = 0; i < keys5.length; i++) {
-  // Add 'open' class on mouseover
-  keys5[i].addEventListener("mousedown", function () {
-    this.classList.add("active");
-  });
-
-  // Remove 'open" class with a delay
-  keys5[i].addEventListener("mouseup", function () {
-    let node = this; // Allows us to access 'this' within the timeout function
-    setTimeout(function () {
-      node.classList.remove("active");
-    }, 300);
-  });
-
-  keys5[i].addEventListener("mouseout", function () {
-    let node = this; // Allows us to access 'this' within the timeout function
+  // Remove 'open" class with a delay after mouseout
+  keys[i].addEventListener("mouseout", function () {
+    let node = this; // Allow access to 'this' in a timeout function
     setTimeout(function () {
       node.classList.remove("active");
     }, 300);
@@ -136,4 +78,19 @@ function playHappyBirthday() {
   time = 0;
 }
 
-// playHappyBirthday();
+function playSong(song) {
+  let time = 0; //Time between notes reset
+  for (let i = 0; i < song.length; i++) {
+    //sso = Song String Octave
+    let sso = song[i].substr(0, 1);
+    //ssnote = Song String Note
+    let ssnote = song[i].substr(1, 3);
+    time = time + 600; //add time between notes
+    setTimeout(() => {
+      Synth.play(0, ssnote, sso, 2);
+    }, time);
+  }
+  time = 0;
+}
+
+playSong(happyBirthdaySong);
