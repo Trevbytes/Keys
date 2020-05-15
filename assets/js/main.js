6badd818bc04aaedf549c01c0f4c011941ceb7a9
Trevbytes <trevor.j.adams@outlook.com>
2020-05-15 09:46:29 +0000

    Added active class during song playback

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const keys = document.querySelectorAll(".key");
 
 keys.forEach((key) => {
   key.addEventListener("mousedown", () =>
-    Synth.play(0, key.id.substr(1, 3), key.id.substr(0, 1), 2)
+    Synth.play(0, key.id.substr(1, 2), key.id.substr(0, 1), 2)
   );
 });
 
@@ -30,6 +30,9 @@ for (let i = 0; i < keys.length; i++) {
   });
 }
 
+// function addActiveClass(){
+
+// };
 /*--Happy Birday song --*/
 
 var happyBirthdaySong = [
@@ -65,7 +68,6 @@ var happyBirthdaySong = [
 var theSong;
 function playSong(song) {
   let i = 0; //index
-  console.log(theSong);
   if (theSong === undefined) {
     theSong = setInterval(function () {
       if (i === song.length) {
@@ -74,8 +76,15 @@ function playSong(song) {
         //sso = Song String Octave
         let sso = song[i].substr(0, 1);
         //ssnote = Song String Note
-        let ssnote = song[i].substr(1, 3);
+        let ssnote = song[i].substr(1, 2);
         Synth.play(0, ssnote, sso, 2);
+
+        let songID = document.getElementById(song[i]);
+        songID.classList.add("active-comp");
+        setTimeout(function () {
+          $(songID).removeClass("active-comp");
+        }, 300);
+
         i++;
       }
     }, 600);
@@ -88,9 +97,16 @@ function playSong(song) {
         //sso = Song String Octave
         let sso = song[i].substr(0, 1);
         //ssnote = Song String Note
-        let ssnote = song[i].substr(1, 3);
+        let ssnote = song[i].substr(1, 2);
 
         Synth.play(0, ssnote, sso, 2);
+
+        let songID = document.getElementById(song[i]);
+        songID.classList.add("active-comp");
+        setTimeout(function () {
+          $(songID).removeClass("active-comp");
+        }, 300);
+
         i++;
       }
     }, 600);
@@ -99,4 +115,38 @@ function playSong(song) {
 
 function reset() {
   clearInterval(theSong);
+}
+
+function teach(song) {
+  let gamelengthindex = 0;
+  while (gamelengthindex < song.length) {
+    let i = 0; //index
+    let sso = song[i].substr(0, 1);
+    //ssnote = Song String Note
+    let ssnote = song[i].substr(1, 2);
+    Synth.play(0, ssnote, sso, 2);
+    let songID = document.getElementById(song[i]);
+    songID.classList.add("active-comp");
+    setTimeout(function () {
+      $(songID).removeClass("active-comp");
+    }, 500);
+
+    $(songID).click(function (event) {
+      // Don't follow the link
+      event.preventDefault();
+
+      // Log the clicked element in the console
+      i++;
+      let sso = song[i].substr(0, 1);
+      //ssnote = Song String Note
+      let ssnote = song[i].substr(1, 2);
+      Synth.play(0, ssnote, sso, 2);
+      let songID = document.getElementById(song[i]);
+      songID.classList.add("active-comp");
+      setTimeout(function () {
+        $(songID).removeClass("active-comp");
+      }, 500);
+      console.log(event.target);
+    });
+  }
 }
